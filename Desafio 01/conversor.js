0d51f770bb4714d747a9f9b305b2639baae665b9
Davi Soares <davisoares94@icloud.com>
2021-03-28 21:09:51 -0300

    Update
    Up

--- Desafio 01/conversor.js
+++ Desafio 01/conversor.js
@@ -1,8 +1,8 @@
-var valorEmDolar = prompt("Qual o valor em dolar que você deseja converter?")
+var valorEmDolar = prompt("Qual o valor em dolar que você deseja realizar a conversão?")
 
-var valorEmDolarNum = parseFloat(valorEmDolarTexto)
+var valorEmDolarNum = parseFloat(valorEmDolar)
 
 var valorEmReal = valorEmDolarNum * 5.76
 var valorEmRealConvertido = valorEmReal.toFixed(2)
 
-alert(valorEmRealFixado)
+alert("A conversão é: " + valorEmRealConvertido)
